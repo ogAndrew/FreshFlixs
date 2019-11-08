988eeddae2c55ffe6bfa7b8a458623749ab9808b
Andrew Talle <andrew.talle@ibm.com>
2019-11-07 18:27:25 -0800

    edit user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   validates :password, length: { minimum: 10, allow_blank: true }
 
+  validates :username, format: { with: /\A[A-Z0-9]+\z/i },
+                     uniqueness: { case_sensitive: false }
+
+
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
