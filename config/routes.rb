5a47766fe5188f54b54a9959b686bc5f937beaec
Andrew Talle <andrew.talle@ibm.com>
2019-11-11 12:37:16 -0800

    add more associations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "movies#index"
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 
   resources :users
